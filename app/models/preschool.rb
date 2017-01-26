77a5ee137cd247fa3d08ccea0937f8724eec3b19
joenas <jonwestin@gmail.com>
2017-01-26 17:43:26 +0100

    Fixed deprecations

--- app/models/preschool.rb
+++ app/models/preschool.rb
@@ -1,5 +1,8 @@
 class Preschool < ActiveRecord::Base
 
+  # TODO to get rid of warnings, change to attribute :position, :point
+  attribute :position, :legacy_point
+
   has_many :hours
 
   validates_presence_of :name
@@ -8,41 +11,8 @@ class Preschool < ActiveRecord::Base
   validates_presence_of :postal_code
   validates_presence_of :city
 
-  scope :with_todays_hours, ->{
-    find_by_sql(with_todays_hours_query)
-  }
-
   def address_params
     attributes.slice('street_name', 'postal_code', 'city').values.join(',') << ",Sweden"
   end
 
-  private
-
-
-  def self.with_todays_hours_query
-    <<-EOF
-    WITH data AS (
-      SELECT
-        preschool_id,
-        make_timestamp(date_part('year', NOW())::int,date_part('month', NOW())::int,date_part('day', NOW())::int,date_part('hours', hours.closes)::int,date_part('minutes', hours.closes)::int,0) #{timezone_cast} as closes,
-        hours.opens <= (now() #{timezone_cast})::time AND hours.closes >= (now() #{timezone_cast})::time AS is_open
-      FROM hours
-      WHERE true
-        AND hours.day_of_week = extract(dow from current_date)
-    )
-    SELECT
-    preschools.*,
-    data.closes,
-    COALESCE(data.is_open, false) as is_open
-    FROM preschools
-    LEFT JOIN data ON data.preschool_id = preschools.id AND data.is_open
-    WHERE true
-
-    ORDER BY COALESCE(data.is_open, false) DESC, data.closes DESC
-    EOF
-  end
-
-  def self.timezone_cast
-    "AT TIME ZONE '#{Time.zone.name}'"
-  end
 end
