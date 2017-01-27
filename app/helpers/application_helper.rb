c16ac4ebd4e05028cdc361c664b87cc86efa146e
joenas <jonwestin@gmail.com>
2017-01-27 16:28:08 +0100

    Added icons for homepage etc, orange color for "info" class and bs variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,23 @@ module ApplicationHelper
       'danger'
     end
   end
+
+  def preschool_closes_class_debug(preschool)
+    #return ['warning', 'info', 'danger', 'success'].sample
+    debug_time_now = Time.new(2017,01,27,13,45,00)
+    last = preschool.hours.today.last.closes
+    debug_time = Time.new(2017,01,27,last.hour,last.min,00)
+    diff = (debug_time - debug_time_now).to_f/3600
+    case diff
+    when (2.0)..(12.0)
+      'success'
+    when (1.0)..(2.0)
+      'info'
+    when (0.5)..(1.0)
+      'warning'
+    when (0.0)..(0.5)
+      'danger'
+    end
+  end
+
 end
