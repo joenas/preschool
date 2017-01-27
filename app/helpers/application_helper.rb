5886be9c5729757ddb8772188e1f73a355603a47
joenas <jonwestin@gmail.com>
2017-01-27 12:10:59 +0100

    Updated design!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # God help me
+  def preschool_closes_class(preschool)
+    return 'default' unless preschool.is_open
+    diff = (preschool.closes - Time.current).to_f/3600
+    case diff
+    when (2.0)..(12.0)
+      'success'
+    when (1.0)..(2.0)
+      'info'
+    when (0.5)..(1.0)
+      'warning'
+    when (0.0)..(0.5)
+      'danger'
+    end
+  end
 end
