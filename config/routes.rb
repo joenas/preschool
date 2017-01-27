ecc2aaecffab43b0f9c838be378ad32aaf89956f
joenas <jonwestin@gmail.com>
2017-01-28 00:34:36 +0100

    Default route for /admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :changes
     resources :preschools
+    root to: 'preschools#index'
   end
 
   get 'position/:latitude,:longitude', to: 'home#index', latitude: /\d+\.\d+/, longitude: /\d+\.\d+/
