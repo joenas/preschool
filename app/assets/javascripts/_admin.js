d34d78243cda224ed08706f1cddea50c8e7d8b37
joenas <jon@jonnev.se>
2017-09-07 17:57:10 +0200

    Better?

--- app/assets/javascripts/_admin.js
+++ app/assets/javascripts/_admin.js
@@ -1,3 +1,27 @@
+jQuery.fn.best_in_place = function () {
+    'use strict';
+    function setBestInPlace(element) {
+        if (!element.data('bestInPlaceEditor')) {
+            element.data('bestInPlaceEditor', new BestInPlaceEditor(element));
+            return true;
+        }
+    }
+
+    jQuery(this.context).delegate(this.selector, 'click', function () {
+        var el = jQuery(this);
+        if (setBestInPlace(el)) {
+            el.click();
+        }
+    });
+
+    this.each(function () {
+        setBestInPlace(jQuery(this));
+    });
+
+    return this;
+};
+
+
 $(document).ready(function() {
   jQuery(".best_in_place").best_in_place();
 });
