75c95bf405d16ea53ef48e407f495b18bc7c15fb
joenas <jon@jonnev.se>
2018-09-12 08:22:43 +0200

    No more static attributes

--- spec/factories/preschools.rb
+++ spec/factories/preschools.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :preschool do
-    name 'Some name'
-    url 'http://example.com'
-    street_name 'Gatan 1'
-    postal_code '13370'
-    city 'Staden'
+    name { 'Some name' }
+    url { 'http://example.com' }
+    street_name { 'Gatan 1' }
+    postal_code { '13370' }
+    city { 'Staden' }
   end
 end
