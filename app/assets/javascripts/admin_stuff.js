60c64d3012ce41698f7f51b4477ba0b1d06fa136
joenas <jon@jonnev.se>
2019-11-05 22:38:53 +0100

    Set minDate on closing time datepicker for admin, also prefills selected time for easier admin

--- app/assets/javascripts/admin_stuff.js
+++ app/assets/javascripts/admin_stuff.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  jQuery(".best_in_place").best_in_place();
-  jQuery(".datetimepicker").datetimepicker({
+  $(".best_in_place").best_in_place();
+  $(".datetimepicker").datetimepicker({
     locale: "sv",
     format: "YYYY-MM-DD HH:mm"
+  });
+  $("#datetimepicker-opens").on("change.datetimepicker", event => {
+    $("#datetimepicker-closes").datetimepicker("minDate", event.date);
   });
 });
